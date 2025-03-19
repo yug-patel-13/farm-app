3043bd05bf403d4b8f210d68ba3aa38a5338abb4
yug-patel-13 <yugpatelart@gmail.com>
2025-03-19 20:27:16 +0530

    all backend and package

--- web-back/index.js
+++ web-back/index.js
@@ -3,8 +3,9 @@ const port = 4000;
 
 // Route to display message when accessing the root URL
 app.get("/", (req, res) => {
-  res.send("Hello, running!");
-});
+    res.send("Hello, running!");
+  });
+  
 
 // Start the server
 app.listen(port, () => console.log(`Server running on port ${port}`));
